2e86a071bea0d042a9322648f5127ec4beec8a80
markO <mark.bus.it@gmail.com>
2019-01-03 22:07:54 +0100

    Initial commit

---
+++ src/main/java/com/example/jms/Application.java
@@ -0,0 +1,32 @@
+package com.example.jms;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+@EnableJms
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+        System.out.println("Sending an email message.");
+        jmsTemplate.convertAndSend("order-queue", "Hello!");
+    }
+
+    @Bean
+    public JmsListenerContainerFactory myFactory(ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        return factory;
+    }
+}
---
+++ src/main/java/com/example/jms/Reciver.java
@@ -0,0 +1,13 @@
+package com.example.jms;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Reciver {
+
+    @JmsListener(destination = "order-queue", containerFactory = "myFactory")
+    public void reciveMessage(String message) {
+        System.out.println("Message: " + message);
+    }
+}
